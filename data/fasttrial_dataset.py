ccacf6778ce227a71d89f70cfb5e06563607b794
rvecchiarelli <rvecchiarell@student.ethz.ch>
2025-03-23 13:29:22 -0400

    Update fasttrial_dataset.py

--- data/fasttrial_dataset.py
+++ data/fasttrial_dataset.py
@@ -22,7 +22,7 @@ import numpy as np
 
 
 
-class ThreeDUVWDataset(BaseDataset):
+class Fasttrial(BaseDataset):
     """A template dataset class for you to implement custom datasets."""
     @staticmethod
     def modify_commandline_options(parser, is_train):
